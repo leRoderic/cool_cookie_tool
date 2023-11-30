f493e4dfa48fd7863a140621186b851579c00f01
Victor Aljaro <49449038+valjaro@users.noreply.github.com>
2023-11-30 20:39:04 +0100

    reading url and first check

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const gdprStrategies = {
     allowAll: "Allow all",
     functionalOnly: "Functional only",
 }
-
+const csvFileUrl = 'https://raw.githubusercontent.com/jkwakman/Open-Cookie-Database/master/open-cookie-database.csv';
 function extractHostname(url) {
     var hostname;
     //find & remove protocol (http, ftp, etc.) and get hostname
@@ -37,8 +37,42 @@ function countElementsArrByKey(arr, key) {
     });
     return res;
 }
+function processData(csvData) {
+    var lines = csvData.split('\n');
+    var result = [];
+    var headers = lines[0].split(',');
+    var cookieMap = new Map();
+    for (var i = 1; i < lines.length; i++) {
+      var obj = {};
+      var currentline = lines[i].split(',');
+      for (var j = 0; j < headers.length; j++) {
+        obj[headers[j]] = currentline[j];
+      }
+      //result.push(obj);
+      cookieMap.set(obj['ID'], obj)
+    }
+    // console.log(cookieMap);
+    return cookieMap
+}
+
 
 export default function Home() {
+    (async () => {
+        try {
+          const csv_data = await fetch(csvFileUrl)
+            .then(response => response.text())
+            .then(res => processData(res))
+            .catch(error => {
+              console.error('Error fetching or processing the file:', error);
+              // Handle the error
+              throw error; // Rethrow the error to ensure 'done' is logged
+            });
+      
+          console.log('Processed CSV data:', csv_data);
+        } finally {
+          console.log('done');
+        }
+      })();
 
     const [cookies, setCookies] = useState([{name: "test", purpose: "test", domain: "test", value: "test"}])
     const [gdprStrategy, setGdprStrategy] = useState("allowAll") // allowAll, functionalOnly,
@@ -60,6 +94,19 @@ export default function Home() {
     useEffect(() => {
         //loadCookies()
     }, []);
+    // if (csv_data.has("f42b671a-b7ba-4e34-a886-6fbb1705d979")){
+    //     console.log("AAAAAA")
+    // }else{
+    //     console.log("BBBBB")
+    // }
+    const checkCSV = (name) => {
+        console.log('In function');
+        if (name == 'twitch.lohp.countryCode') {
+          return true;
+        } else {
+          return false;
+        }
+    };
     return (
         <div style={{backgroundColor: "white"}}>
             <Head>
@@ -106,6 +153,7 @@ export default function Home() {
                                 <th style={{maxWidth: "30px", wordWrap: "break-word"}}>Name</th>
                                 <th>Purpose</th>
                                 <th>Domain</th>
+                                <th>in CSV</th>
                                 <th></th>
                             </tr>
                             </thead>
@@ -114,6 +162,7 @@ export default function Home() {
                                     <td>{cookie.name}</td>
                                     <td>{cookie.purpose}</td>
                                     <td>{cookie.domain}</td>
+                                    <td>{checkCSV(cookie.name) ? 'Yes' : 'No'}</td>
                                     <td>
                                         <OverlayTrigger overlay={<div style={{color: "black"}}>{cookie.value}</div>}
                                                         placement={"bottom"}>
