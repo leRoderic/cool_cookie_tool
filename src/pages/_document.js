f9f5acf44839095aeb50aac296c1d1bda8238b8d
Rodrigo Cabezas Quirós <rcmikado@gmail.com>
2024-01-13 15:04:52 +0100

    updated README, added license, solved manifest errors

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,20 +3,11 @@ import {Html, Head, Main, NextScript} from 'next/document'
 export default function Document() {
     return (
         <Html lang="en">
-            <link
-                rel="stylesheet"
-                href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-                integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
-                crossOrigin="anonymous"
-            />
             <Head/>
             <body>
             <Main/>
             <NextScript/>
             </body>
-            <script
-                src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js"
-                crossOrigin></script>
         </Html>
     )
 }
